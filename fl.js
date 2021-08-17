c61a11eed9d41696126b8c17c7ce427a40f934db
myNameIsPK <43566401+myNameIsPK@users.noreply.github.com>
2021-08-17 23:04:35 +0700

    fix layout

--- fl.js
+++ fl.js
@@ -9,9 +9,9 @@ let svgLayer = L.svg();
 svgLayer.addTo(map)
 
 let radius = 10;
-let xarr = [100, 200, 300]
 
 d3.json("data.json").then(data => {
+
   data.nodes.forEach((d, i) => {
     d.latLong = new L.LatLng(d.lat, d.lon);
     d.layerPoint = map.latLngToLayerPoint(d.latLong)
@@ -24,50 +24,56 @@ d3.json("data.json").then(data => {
     d.source = d.from;
     d.target = d.to;
   });
-  
-  const ticked = () => {
-    // const line = d3.select("body>svg")
-    const line = d3.select("#map").select("svg").select("g")
-    .selectAll("line")
+
+  const svg = d3.select("#map").select("svg")
+  const g = svg.select("g")
+
+  const lines = g .selectAll("line")
     .data(data.links)
     .join("line")
     .attr("stroke", "red")
     .attr("stroke-opacity", 0.3)
     .attr("stroke-width", 5)
-    .attr("x1", (d) => d.source.x)
-    .attr("y1", (d) => d.source.y)
-    .attr("x2", (d) => d.target.x)
-    .attr("y2", (d) => d.target.y)
+
   
-    // const circle = d3.select("body>svg")
-    const circle = d3.select("#map").select("svg").select("g")
-      .selectAll("circle")
-      .data(data.nodes)
-      .join("circle")
-      .attr("r", radius)
-      .attr("fill", "blue")
+  const nodes = g.selectAll("circle")
+    .data(data.nodes)
+    .join("circle")
+    .attr("r", radius)
+    .attr("fill", "blue")
+    .each((d) => {
+      // fix parent node position by set fx and fy, unfix by set it to null
+      if (d.type === "parent"){
+        d.fx = d.layerPoint.x
+        d.fy = d.layerPoint.y
+      }
+    })
+  
+  const drawAndUpdate = () => {
+
+    lines
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y)
+  
+    nodes
       .attr("cx", d => d.x)
       .attr("cy", d => d.y)
-      .each((d) => {
-        if (d.type === "parent"){
-          d.fx = d.layerPoint.x
-          d.fy = d.layerPoint.y
-        }
-      })
   }
 
   const simulation = d3.forceSimulation(data.nodes)
     .force('link', d3.forceLink().links(data.links).id(d => d.id))
-    // .force('link', d3.forceLink().links(data.links).id(d => d.id).distance(100))
-    // .force('charge', d3.forceManyBody().strength(5))
+    // .force('link', d3.forceLink().links(data.links).id(d => d.id).distance(50))
     .force('charge', d3.forceManyBody())
+    // .force('charge', d3.forceManyBody().strength(5))
     .force('collision', d3.forceCollide().radius(radius))
     .force('x', d3.forceX().x(d => d.layerPoint.x))
     .force('y', d3.forceY().y(d => d.layerPoint.y))
     // .force('x', d3.forceX().x(d => d.x).strength(0.06))
     // .force('y', d3.forceY().y(d => d.y).strength(0.04))
     .on('tick', () => {
-      ticked()
+      drawAndUpdate()
     })
 
 })
